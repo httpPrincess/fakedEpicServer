778600ce2fdcb8cf754a7dff3ddd820e5669c779
jj <ryba271+git@google.com>
2014-04-17 10:31:36 +0200

    behaves like sara epic

--- simpleEpic.py
+++ simpleEpic.py
@@ -2,8 +2,10 @@ import flask
 from flask import Flask
 from flask import request
 from flask import url_for
+from random import randint
 
 import json
+import uuid
 
 app = Flask(__name__)
 
@@ -19,14 +21,12 @@ def extract_suffix(pid):
 
 
 def get_prefixes(pids):
-    return map(extract_prefix, pids)
+    return list(set(map(extract_prefix, pids)))
 
 
 def get_suffixes(pids, prefix):
     return {extract_suffix(k) for k, v in pids.iteritems() if k.startswith(prefix)}
 
-
-#{k: v for k,v in pids.iteritems() if k.count('8444/')}
 
 @app.route('/', methods=['GET'])
 def get_prefix_list():
@@ -56,12 +56,45 @@ def add_pid(prefix, suffix):
         'Location': url_for('get_pid', prefix=prefix, suffix=suffix)}
 
 
+def generate_suffix():
+    return str(uuid.uuid1())
+
+
+@app.route('/<prefix>/', methods=['POST'])
+def create_pid(prefix):
+    condition = True
+    while condition:
+        suffix = generate_suffix()
+        pid = '%s/%s' % (prefix, suffix)
+        condition = pids.has_key(pid)
+
+    pids[pid] = request.data
+    return ('You updated pid %s\nValue:%s\n' % (pid, request.data)), 201, {
+        'Location': url_for('get_pid', prefix=prefix, suffix=suffix)}
+
+
+def convert_to_handle(location, checksum):
+    if checksum:
+        new_handle_json = json.dumps([{'type': 'URL',
+                                             'parsed_data': location},
+                                            {'type': 'CHECKSUM',
+                                             'parsed_data': str(checksum)}])
+    else:
+        new_handle_json = json.dumps([{'type': 'URL',
+                                             'parsed_data': location}])
+    return new_handle_json
+
+
 @app.before_first_request
 def populate_pids():
-    pids['8444/21211'] = "{'key':'value', 'key2':'value2'}"
-    pids['85411/dasa'] = "{'ada':'daa', 'foo':'bar'}"
-    pids['85444/foobar'] = "{'key1':'value1', 'key2':'value2'}"
-
+    prefixes = ['8441', '90210', '44']
+    location_string = 'irods://zone:1247/home/eudat/'
+    for prefix in prefixes:
+        for i in range(1, randint(5, 10)):
+            suffix = generate_suffix()
+            location = '%s/%s' % (location_string, suffix)
+            checksum = str(uuid.uuid1())
+            pids['%s/%s' % (prefix, suffix)] = convert_to_handle(location=location, checksum=checksum)
 
 if __name__ == '__main__':
     app.run(debug=True)
