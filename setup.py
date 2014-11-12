96c8a2238a1dbc98550d060ef9d14f4b25a7d112
httpPrincess <ryba271+git@google.com>
2014-11-12 16:34:42 +0100

    setup added for automatic updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='Mock Simple Epic',
+    version='0.1.0',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['Flask'], 
+)
